51c948b4f2d69cccb2231156fabe5e5f0d3c589a
Jack Hayter <jack.hayter@googlemail.com>
2017-04-04 19:27:50 -0700

    Improvements to handling of status codes

--- lib/sockeye/client.rb
+++ lib/sockeye/client.rb
@@ -6,49 +6,55 @@ module Sockeye
 
     attr_accessor :server_address, :auth_token, :on_message, :on_error, :on_close
 
-    def initialize(server_address:, auth_token:, on_message:, on_error:, on_close:)
+    def initialize(server_address:, auth_token:, on_message: nil, on_error: nil, on_close: nil)
       self.server_address = server_address
-      self.auth_token = auth_token
-      self.on_message = on_message
-      self.on_error = on_error
-      self.on_close = on_close
+      self.auth_token     = auth_token
+      self.on_message     = on_message || Proc.new { |payload| nil }
+      self.on_error       = on_error   || Proc.new { |payload| nil }
+      self.on_close       = on_close   || Proc.new { nil }
+    end
+
+    @raw_socket = nil 
+
+    def close
+      @raw_socket.close
+      EM.stop
     end
 
     def connect
 
       EM.run do
 
-        ws = WebSocket::EventMachine::Client.connect(uri: self.server_address)
+        @raw_socket = WebSocket::EventMachine::Client.connect(uri: self.server_address)
 
-        ws.onopen do
-          ws.send({action: :authenticate, payload: self.auth_token}.to_json)
+        @raw_socket.onopen do
+          @raw_socket.send({action: :authenticate, payload: self.auth_token}.to_json)
         end
 
         # When a message is received, ensure the server sent a valid payload
         # then handle it based on status code.
         #
-        ws.onmessage do |msg, type|
+        @raw_socket.onmessage do |msg, type|
           message = json_try_parse(msg)
-          puts message.inspect
           if message.nil? || message[:payload].nil? || message[:status].nil?
             puts "Invalid message"
             self.on_error.call
             ws.close
-          elsif message[:status] != 200
-            self.on_error.call(message[:status])
+          elsif message[:status].to_i >= 400 || message[:status].to_i == 0
+            self.on_error.call(message)
           else
-            self.on_message.call(message[:payload])
+            self.on_message.call(message)
           end
         end
 
-        ws.onclose do
+        @raw_socket.onclose do
           self.on_close.call
           EM.stop
         end
 
-        ws.onerror do
+        @raw_socket.onerror do
           self.on_error.call(0)
-          ws.close
+          @raw_socket.close
           EM.stop
         end
 
